7c8012c57c4653d6ded2eea348c637fea2dfb871
ErinKace <erinkacerovskis@gmail.com>
2023-06-07 18:59:04 -0500

    got a basic outline

---
+++ src/main/java/org/restaurantmenu/java/Menu.java
@@ -0,0 +1,14 @@
+package org.restaurantmenu.java;
+
+public class Menu {
+    // private ArrayList<MenuItem> menuItems
+    // private Date dateLastUpdated
+    // public Menu() {
+    // }
+
+    // public ArrayList<MenuItem> getMenuItems
+    // public Date getDateLastUpdated
+    // public void addMenuItem
+    // public void removeMenuItem
+    // private void setDateLastUpdated
+}
---
+++ src/main/java/org/restaurantmenu/java/MenuItem.java
@@ -0,0 +1,19 @@
+package org.restaurantmenu.java;
+
+public class MenuItem {
+
+    // private String name
+    // private Double price
+    // private String category - String
+    // private String description - String
+    // private boolean newItem
+
+    // public String getName
+    // public Double getPrice
+    // public String getCategory
+    // public String getDescription
+    // private void setName
+    // private void setPrice
+    // public void setCategory
+    // public void setDescription
+}
