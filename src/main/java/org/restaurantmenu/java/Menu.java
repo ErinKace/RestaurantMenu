b4cb9cbea05ce933a7355f70564f49a7dc5efee4
ErinKace <erinkacerovskis@gmail.com>
2023-06-07 19:41:27 -0500

    Finished the actual code

--- src/main/java/org/restaurantmenu/java/Menu.java
+++ src/main/java/org/restaurantmenu/java/Menu.java
@@ -1,14 +1,42 @@
 package org.restaurantmenu.java;
 
-public class Menu {
-    // private ArrayList<MenuItem> menuItems
-    // private Date dateLastUpdated
-    // public Menu() {
-    // }
+import java.util.ArrayList;
+import java.util.Date;
 
-    // public ArrayList<MenuItem> getMenuItems
-    // public Date getDateLastUpdated
-    // public void addMenuItem
-    // public void removeMenuItem
-    // private void setDateLastUpdated
+public class Menu {
+     private ArrayList<MenuItem> menuItems = new ArrayList<>();
+     private Date dateLastUpdated;
+     public Menu() {
+         this.dateLastUpdated = new Date();
+     }
+     public Menu(ArrayList<MenuItem> newMenuList) {
+         this.menuItems = newMenuList;
+         this.dateLastUpdated = new Date();
+     }
+
+     public ArrayList<MenuItem> getMenuItems() {
+         return this.menuItems;
+     }
+     public Date getDateLastUpdated() {
+         return this.dateLastUpdated;
+     }
+     public void addMenuItem(MenuItem newMenuItem) {
+         this.menuItems.add(newMenuItem);
+         this.dateLastUpdated = new Date();
+     }
+     public void removeMenuItem(String itemToRemove) {
+         MenuItem objectToRemove;
+         for (MenuItem item : this.menuItems) {
+             if (item.getName().equals(itemToRemove)) {
+                 objectToRemove = item;
+                 int itemIndex = this.menuItems.indexOf(objectToRemove);
+                 this.menuItems.remove(itemIndex);
+             } else {
+                 System.out.println("Item not found");
+             }
+         }
+     }
+     private void setDateLastUpdated() {
+         this.dateLastUpdated = new Date();
+     }
 }
--- src/main/java/org/restaurantmenu/java/MenuItem.java
+++ src/main/java/org/restaurantmenu/java/MenuItem.java
@@ -2,18 +2,47 @@ package org.restaurantmenu.java;
 
 public class MenuItem {
 
-    // private String name
-    // private Double price
-    // private String category - String
-    // private String description - String
-    // private boolean newItem
+     private String name;
+     private Double price;
+     private String category;
+     private String description;
+     private boolean newItem;
 
-    // public String getName
-    // public Double getPrice
-    // public String getCategory
-    // public String getDescription
-    // private void setName
-    // private void setPrice
-    // public void setCategory
-    // public void setDescription
+     public MenuItem(String aName, Double aPrice, String aCategory, String aDescription) {
+         this.name = aName;
+         this.price = aPrice;
+         this.category = aCategory;
+         this.description = aDescription;
+         this.newItem = true;
+     }
+     public String getName() {
+         return this.name;
+     }
+     public Double getPrice() {
+         return this.price;
+     }
+     public String getCategory() {
+         return this.category;
+     }
+     public String getDescription() {
+         return this.description;
+     }
+     public boolean getNewItem() {
+         return this.newItem;
+     }
+     private void setName(String newName) {
+         this.name = newName;
+     }
+     private void setPrice(Double newPrice) {
+         this.price = newPrice;
+     }
+     public void setCategory(String newCategory) {
+         this.category = newCategory;
+     }
+     public void setDescription(String newDescription) {
+         this.description = newDescription;
+     }
+     public void setNewItem() {
+         this.newItem = false;
+     }
 }
